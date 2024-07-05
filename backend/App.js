380cb4998fadb07ed894876a31027b6f3a744237
Amna Khan <amnakhan@Amnas-MacBook-Pro.local>
2024-07-05 16:51:22 -0500

    added express routing

--- backend/App.js
+++ backend/App.js
@@ -1,13 +1,16 @@
 const { createServer } = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+//const hostname = '127.0.0.1';
+const express = require('express')
+const app = express();
+const port = 8000;
+
+
+
+app.get('/', (req, res) => {
+res.status(200).send("hello world");
+})
 
 // set up server
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port,  () => {
+  console.log(`Server running at http://:${port}/`);
 });
